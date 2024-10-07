8f47623dbbd5391f528ee81abec04028f0927810
Murilo Dorigo. <murillodorigo@gmail.com>
2024-10-07 18:24:00 -0300

    feat: adicionado função para visualizar o stock

--- djangoapp/apps/stock/views.py
+++ djangoapp/apps/stock/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.contrib import messages
+from django.shortcuts import get_object_or_404, redirect
 # Create your views here.
+def index(request):
+    if not request.user.is_authenticated:
+        messages.error(request, 'You are not logged in!')
+        return redirect('login_user')
+    return render(request, 'stock/index.html')
