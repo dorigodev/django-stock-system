3fd270c7fa4e9e4764b76700efb7b2f2015ad81d
Murilo Dorigo. <murillodorigo@gmail.com>
2024-10-08 10:13:43 -0300

    fix: consertando nomes dos endpoints

--- djangoapp/apps/users/urls.py
+++ djangoapp/apps/users/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from django.urls import include
 from apps.users.views import create_User, login_User, logout_User
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('register_store', create_User, name="create_user"),
-    path('login_user', login_User, name="login_user"),
+    path('register', create_User, name="create_user"),
+    path('login', login_User, name="login_user"),
     path('logout', logout_User, name='logout'),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
