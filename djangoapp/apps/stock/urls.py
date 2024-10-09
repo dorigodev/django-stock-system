ecd7672ec0ed321a93e655ac4d54e8c3c25640fc
Murilo Dorigo. <murillodorigo@gmail.com>
2024-10-09 11:16:28 -0300

    feat: adicionado endpoints pro update, delete e search

--- djangoapp/apps/stock/urls.py
+++ djangoapp/apps/stock/urls.py
@@ -5,10 +5,16 @@ from django.conf.urls.static import static
 from django.conf import settings
 from apps.stock.views import index
 from apps.stock.views import create_product
+from apps.stock.views import update_product
+from apps.stock.views import delete_product
+from apps.stock.views import search_product
 
 urlpatterns = [
     path('', index, name='index'),
     path('create_product', create_product, name='create_product'),
+    path('update_product/<int:product_id>', update_product, name='update_product'),
+    path('delete_product/<int:product_id>', delete_product, name='delete_product'),
+    path('search_product', search_product, name='search_product'),
 ]
 if settings.DEBUG:
     urlpatterns += static(
